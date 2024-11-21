150e99507ec52e9ae73ab7b209b0ee84a144699b
kyle-j-fender <kylejfender@gmail.com>
2024-11-21 00:06:42 -0500

    Changes to Include SHP and Naming Convention

--- main.py
+++ main.py
@@ -15,24 +15,32 @@ if __name__ == '__main__':
     st.sidebar.markdown("# User Input")
     col1, col2, col3 = st.columns([1, 1, 1])
 
-    df_on = st.sidebar.toggle("Determining Factor")
+    shc_on = st.sidebar.toggle("Sprinkler Head (Sq. Footage)")
+    if shc_on:
+        sprinkler_head_pressure = st.sidebar.number_input("Sprinkler Head Coverage", value=5)
+
+    density_on = st.sidebar.toggle("Density")
+    if density_on:
+        density = st.sidebar.number_input("Density", value=0.05)
+
+    df_on = st.sidebar.toggle("K-Factor")
     if df_on:
-        determining_factor = st.sidebar.number_input("Determining Factor", value=5.6)
+        determining_factor = st.sidebar.number_input("K-Factor", value=5.6)
 
-    flow_on = st.sidebar.toggle("Flow")
+    flow_on = st.sidebar.toggle("Q")
     if flow_on:
-        flow = st.sidebar.number_input("Flow", value=22.5)
+        flow = st.sidebar.number_input("Q", value=22.5)
 
-    rp_on = st.sidebar.toggle("Required Pressure")
+    rp_on = st.sidebar.toggle("P (Required Pressure)")
     if rp_on:
         required_pressure = st.sidebar.number_input("Required Pressure", value=16.14)
         square_root_rp = math.sqrt(required_pressure)
 
-    col1.subheader("DF", divider = True)
+    col1.subheader("K-Factor", divider = True)
     col1.caption("The equivalent K at a node.")
-    col2.subheader("Flow (gpm)", divider = True)
+    col2.subheader("Q - Flow", divider = True)
     col2.caption("This is the calculated flow.")
-    col3.subheader("RP (psi)", divider = True)
+    col3.subheader("P (Required Pressure)", divider = True)
     col3.caption("This is the pressure at the node.")
 
 
@@ -55,11 +63,16 @@ if __name__ == '__main__':
         det_metric = det_verification_box.metric(label="Match", value=det_metric_calc_formatted, delta=range_des)
 
     sub2_col1, sub2_col2 = col2.columns([1,1])
+    sub2_col1_1, sub2_col2_2 = col2.columns([1,1])
     flow_calculation_box = sub2_col1.container(height=120)
+    flow_shp_box = sub2_col1_1.container(height=120)
+    flow_match_box_shp = sub2_col2_2.container(height=120)
     if df_on & rp_on:
         flow_calc = determining_factor * square_root_rp
         flow_calc_formatted = "{:.3f}".format(flow_calc)
-        flow_display = flow_calculation_box.metric(label="Calculated", value=flow_calc_formatted)
+        flow_display = flow_calculation_box.metric(label="Q (from DF & RP)", value=flow_calc_formatted)
+        flow_calc_shp = sprinkler_head_pressure * density
+        flow_calculation_shp = flow_shp_box.metric(label="Q (from SHP and Density)", value=flow_calc_shp)
     if df_on & flow_on & rp_on:
         flow_verification_box = sub2_col2.container(height=120)
         flow_bound1 = calc.bounds(flow_calc, flow)
@@ -71,13 +84,22 @@ if __name__ == '__main__':
         else:
             range_flow = "Out of Range"
         flow_metric = flow_verification_box.metric(label="Match", value=flow_metric_calc_formatted, delta=range_flow)
+        flow_bound1_shp = calc.bounds(flow_calc_shp, flow)
+        flow_bound2_shp = calc.bounds(flow, flow_calc_shp)
+        flow_metric_calc_formatted_shp = "{:.1f}%".format(flow_bound1_shp)
+        range_flow_shp = "hello"
+        if (98 < flow_bound1_shp < 102) or (98 < flow_bound2_shp < 102):
+            range_flow_shp = "In Range"
+        else:
+            range_flow_shp = "Out of Range"
+        flow_metric_shp = flow_match_box_shp.metric(label="Match", value=flow_metric_calc_formatted_shp, delta=range_flow_shp)
 
     sub3_col1, sub3_col2 = col3.columns([1,1])
     rp_calculation_box = sub3_col1.container(height=120)
     if df_on & flow_on:
         rp_calc = (flow/determining_factor) ** 2
         rp_calc_formatted = "{:.2f}".format(rp_calc)
-        rp_display = rp_calculation_box.metric(label="Calculated", value=rp_calc_formatted)
+        rp_display = rp_calculation_box.metric(label="PSI (from K-Factor & Q)", value=rp_calc_formatted)
     if df_on & flow_on & rp_on:
         rp_verification_box = sub3_col2.container(height=120)
         rp_bound1 = calc.bounds(rp_calc, required_pressure)
